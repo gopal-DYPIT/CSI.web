0109a2b8df98bf111cc1b74272dab6076d6ca326
[Gopal Kadam] <[gopalkadam212@gmail.com]>
2024-09-29 11:28:54 +0530

    navbar responsive

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import { Facebook } from "@mui/icons-material";
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // New state variable
 
   const changeBackground = () => {
     if (window.scrollY >= 50) {
@@ -21,6 +22,14 @@ const Navbar = () => {
     window.scrollTo(0, 0);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", changeBackground);
     return () => {
@@ -30,44 +39,43 @@ const Navbar = () => {
 
   return (
     <nav className={navbar ? "navbar navbar-expand-lg navbar-dark NActive" : "navbar navbar-expand-lg navbar-dark"}>
-      <NavLink className="navbar-brand" to="/" onClick={scrollToTop}>
+      <NavLink className="navbar-brand" to="/" onClick={() => {scrollToTop(); closeMenu();}}>
         <img className="logo_csi" src={logo} alt="CSI Logo" />
       </NavLink>
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
+        onClick={toggleMenu} // Toggle menu open/close
         aria-controls="navbarNav"
-        aria-expanded="false"
+        aria-expanded={isMenuOpen} // Update the expanded state
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse d-flex-lg-row justify-content-lg-around" id="navbarNav">
+      <div className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`} id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <NavLink className="nav-link" exact to="/" activeClassName="active" onClick={scrollToTop}>
+            <NavLink className="nav-link" exact to="/" activeClassName="active" onClick={() => {scrollToTop(); closeMenu();}}>
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/event" activeClassName="active" onClick={scrollToTop}>
+            <NavLink className="nav-link" to="/event" activeClassName="active" onClick={() => {scrollToTop(); closeMenu();}}>
               Events
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/member" activeClassName="active" onClick={scrollToTop}>
+            <NavLink className="nav-link" to="/member" activeClassName="active" onClick={() => {scrollToTop(); closeMenu();}}>
               Members
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/blog" activeClassName="active" onClick={scrollToTop}>
+            <NavLink className="nav-link" to="/blog" activeClassName="active" onClick={() => {scrollToTop(); closeMenu();}}>
               Blogs
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="/about" activeClassName="active" onClick={scrollToTop}>
+            <NavLink className="nav-link" to="/about" activeClassName="active" onClick={() => {scrollToTop(); closeMenu();}}>
               About
             </NavLink>
           </li>
